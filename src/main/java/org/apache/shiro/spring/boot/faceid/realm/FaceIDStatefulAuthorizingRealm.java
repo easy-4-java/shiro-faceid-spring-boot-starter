0b16e97f78593775eafb858517e99c9d90d8ec4b
wandl-6A72h <hiwepy@gmail.com>
2026-08-07 01:53:32 +0800

    docs: 新增中英文双语 README（统一格式）

--- src/main/java/org/apache/shiro/spring/boot/faceid/realm/FaceIDStatefulAuthorizingRealm.java
+++ src/main/java/org/apache/shiro/spring/boot/faceid/realm/FaceIDStatefulAuthorizingRealm.java
@@ -5,7 +5,7 @@ import org.apache.shiro.spring.boot.faceid.token.FaceIDLoginToken;
 
 /**
  * Kisso Stateful AuthorizingRealm
- * @author 		： <a href="https://github.com/hiwepy">hiwepy</a>
+ * @author 		： <a href="https://github.com/easy-4-java">hiwepy</a>
  */
 public class FaceIDStatefulAuthorizingRealm extends AbstractAuthorizingRealm {
 
