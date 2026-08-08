565aaf40682b26f2e6bfbe536c9a053c6180aab9
wandl-6A72h <hiwepy@gmail.com>
2026-08-08 23:57:29 +0800

    chore: propagate pom standardization, tests, javadoc and @author fix from 4.1.x
    - Add jacoco-maven-plugin 0.8.15 with 90% coverage gate
    - Add central-publishing-maven-plugin 0.11.0
    - Update surefire (enable tests, argLine for JaCoCo, *_Test.java pattern)
    - Fix SCM URLs
    - Copy test files from 4.1.x branch
    - Copy Javadoc improvements from 4.1.x branch
    - Standardize @author to [@Loong Wan](https://github.com/loong10k)
    - Propagated to 3.5.x branch

--- src/main/java/org/apache/shiro/spring/boot/faceid/realm/FaceIDStatefulAuthorizingRealm.java
+++ src/main/java/org/apache/shiro/spring/boot/faceid/realm/FaceIDStatefulAuthorizingRealm.java
@@ -5,7 +5,7 @@ import org.apache.shiro.spring.boot.faceid.token.FaceIDLoginToken;
 
 /**
  * Kisso Stateful AuthorizingRealm
- * @author 		： <a href="https://github.com/hiwepy">hiwepy</a>
+ * @author [@Loong Wan](https://github.com/loong10k)
  */
 public class FaceIDStatefulAuthorizingRealm extends AbstractAuthorizingRealm {
 
