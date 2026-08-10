7b663ab3480e986cd6fd0d967f4a9f5cc960997d
loong10k <20489781+loong10k@users.noreply.github.com>
2026-08-10 17:10:44 +0800

    docs: unify @author tag to @author <a href="https://github.com/loong10k">Loong Wan</a>

--- src/main/java/org/apache/shiro/spring/boot/faceid/FaceType.java
+++ src/main/java/org/apache/shiro/spring/boot/faceid/FaceType.java
@@ -17,7 +17,7 @@ package org.apache.shiro.spring.boot.faceid;
 
 /**
  * 人脸的类型
- * @author [@Loong Wan](https://github.com/loong10k)
+ * @author <a href="https://github.com/loong10k">Loong Wan</a>
  */
 public enum FaceType {
 
