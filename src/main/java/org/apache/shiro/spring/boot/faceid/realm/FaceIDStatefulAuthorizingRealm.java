1bd2575d29fb6dee410e5759ab3151940b12654b
loong10k <20489781+loong10k@users.noreply.github.com>
2026-08-10 17:10:25 +0800

    docs: unify @author tag to @author <a href="https://github.com/loong10k">Loong Wan</a>

--- src/main/java/org/apache/shiro/spring/boot/faceid/realm/FaceIDStatefulAuthorizingRealm.java
+++ src/main/java/org/apache/shiro/spring/boot/faceid/realm/FaceIDStatefulAuthorizingRealm.java
@@ -5,7 +5,7 @@ import org.apache.shiro.spring.boot.faceid.token.FaceIDLoginToken;
 
 /**
  * Kisso Stateful AuthorizingRealm
- * @author [@Loong Wan](https://github.com/loong10k)
+ * @author <a href="https://github.com/loong10k">Loong Wan</a>
  */
 public class FaceIDStatefulAuthorizingRealm extends AbstractAuthorizingRealm {
 
